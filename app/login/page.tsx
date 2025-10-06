46359c3daa9c436c3288c57e754461e836524d86
Jaymin Parmar <jaimins100@gmail.com>
2025-10-06 15:50:18 +0530

    auth redirect

---
+++ app/login/page.tsx
@@ -0,0 +1,15 @@
+import {createClient} from "@/supabase/server";
+import {redirect} from "next/navigation";
+import {AuthButton} from "@/app/auth-button";
+
+export default async function Login() {
+    const supabase = await createClient();
+
+    const {data: {session}} = await supabase.auth.getSession()
+
+    if(session) {
+        redirect("/")
+    }
+
+    return <AuthButton session={session} />
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 import {createClient} from "@/supabase/server";
 import {AuthButtonServer} from "@/app/auth-button-server";
+import {redirect} from "next/navigation";
 
 export default async function Home() {
   const supabase = await createClient();
+
+  const {data: {session}} = await supabase.auth.getSession()
+
+  if(!session) {
+    redirect("/login")
+  }
 
   const { data: tweets } = await supabase.from("tweets").select()
 
