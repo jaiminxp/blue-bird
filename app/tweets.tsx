f691165e26e367320d277265a64ada139e57639a
Jaimin Parmar <jaimins100@gmail.com>
2025-11-23 23:36:05 +0530

    listen for realtime database changes

--- app/tweets.tsx
+++ app/tweets.tsx
@@ -1,8 +1,35 @@
 'use client'
 
+import { useEffect } from 'react'
 import Likes from './likes'
+import { createClient } from '@/supabase/client'
+import { useRouter } from 'next/navigation'
+import { RealtimeChannel, SupabaseClient } from '@supabase/supabase-js'
 
 export default function Tweets({ tweets }: { tweets: TweetWithAuthor[] }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    let supabase: SupabaseClient<Database>
+    let channel: RealtimeChannel
+
+    async function refreshOnChange() {
+      supabase = await createClient<Database>()
+      channel = supabase
+        .channel('realtime-tweets')
+        .on('postgres_changes', { event: '*', schema: 'public' }, () => {
+          router.refresh()
+        })
+        .subscribe()
+    }
+
+    refreshOnChange()
+
+    return () => {
+      supabase.removeChannel(channel)
+    }
+  }, [router])
+
   return (
     <div>
       {tweets?.map((tweet) => (
